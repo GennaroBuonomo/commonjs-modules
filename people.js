c153ae7a7027d68b52eab65e600de05664be15a8
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-29 16:40:57 +0200

    feat importo della funzione

---
+++ people.js
@@ -0,0 +1,9 @@
+// definisco una funzione che mi restituisce un oggetto con nome e Hobby
+const peoples = (firstName, hobbyOne) => {
+  return {
+    firstName,
+    hobbyOne
+  }
+}
+
+module.exports = peoples
