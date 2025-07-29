2e8f866bf0a4b8dcf7135d2360ec089124bd7b21
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-29 16:26:14 +0200

    faet definisco una funzione con array di dstringa

---
+++ hobbies.js
@@ -0,0 +1,6 @@
+//definisco una funzione che mi restituisce un array hobbyOne, hobbyTwo, hobbyThree
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+  return {
+    [hobbyOne, hobbyTwo, hobbyThree]   
+  }
+}
