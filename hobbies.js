70609ba3c70bead4e423f6081f733707ed795cdf
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-29 16:29:26 +0200

    fix hobbies

--- hobbies.js
+++ hobbies.js
@@ -1,6 +1,4 @@
 //definisco una funzione che mi restituisce un array hobbyOne, hobbyTwo, hobbyThree
 const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
-  return {
-    [hobbyOne, hobbyTwo, hobbyThree]   
-  }
+  return [hobbyOne, hobbyTwo, hobbyThree]   
 }
