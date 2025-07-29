815388928add358b224f5c26e69383830bb4b42b
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-29 16:21:24 +0200

    feat definisco una funzione di recupero

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // importazione della funzione 
 const subject = require('./name.js')
+// definisco una funzione di recupero
+const getSubjectInfo = () => {
+  return {
+    subjectInfo: subject('Gennaro', 'Buonomo')
+  }
+}
+
+console.log(getSubjectInfo())
